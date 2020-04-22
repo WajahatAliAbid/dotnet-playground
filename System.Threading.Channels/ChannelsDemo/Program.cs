9be482369e174e8cf7d490b114d0921bc3e1311a
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2020-04-22 08:56:42 +0500

    added threading channels demo

---
+++ System.Threading.Channels/ChannelsDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelsDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = Channel.CreateBounded<int>(2);
+            _ = Task.Run(async delegate {
+                for (int i = 0; i < 100; i++)
+                {
+                    await Task.Delay(500);
+                    await channel.Writer.WriteAsync(i);
+                }
+                channel.Writer.Complete();
+            });
+            await foreach (var item in channel.Reader.ReadAllAsync())
+            {
+                Console.WriteLine(item);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
