5c87bbbd40637d21e2c9348573158ffbe8e58fff
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2021-02-13 20:03:03 +0500

    added username not null attribute

--- src/interface-mocking/src/GithubMockService/IGithubService.cs
+++ src/interface-mocking/src/GithubMockService/IGithubService.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using System;
 
 namespace GithubMockService
 {
@@ -9,7 +11,8 @@ namespace GithubMockService
         /// </summary>
         /// <param name="username">case sensitive username</param>
         /// <exception cref="GithubUserNotFoundException">Thrown when user doesn't exist for username</exception>
+        /// <exception cref="ArgumentNullException">Thrown when username is null or empty</exception>
         /// <returns>Returns <see cref="GithubUser"/> object for corresponding username</returns>
-        ValueTask<GithubUser> GetUserAsync(string username);
+        ValueTask<GithubUser> GetUserAsync([NotNull] string username);
     }
 }
