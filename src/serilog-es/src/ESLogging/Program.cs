e5b21db6ab06e4133ec62936605cc95774a9e161
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2021-10-10 14:59:28 +0500

    added microsoft extensions serilog

--- src/serilog-es/src/ESLogging/Program.cs
+++ src/serilog-es/src/ESLogging/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Formatting.Elasticsearch;
 using Serilog.Sinks.Elasticsearch;
@@ -11,33 +11,46 @@ namespace ESLogging
     {
         static async Task Main(string[] args)
         {
-            var loggerConfig = new LoggerConfiguration();
-            loggerConfig.WriteTo
-                .Console(new ElasticsearchJsonFormatter());
-            loggerConfig.WriteTo
-                .Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200/"))
-                {
-                    IndexFormat = "serilog-{0:yyyy}",
-                    NumberOfShards = 3,
-                    NumberOfReplicas = 1,
-                    AutoRegisterTemplate = true,
-                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7, 
-                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
-                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-                                       EmitEventFailureHandling.WriteToFailureSink |
-                                       EmitEventFailureHandling.RaiseCallback,
-                });
-            var logger = loggerConfig.CreateLogger();
+            var logger = LoggerFactory.Create(options => 
+            {
+                var loggerConfig = new LoggerConfiguration();
+                loggerConfig.WriteTo
+                    .Console(new ElasticsearchJsonFormatter());
+                loggerConfig.WriteTo
+                    .Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200/"))
+                    {
+                        IndexFormat = "serilog-{0:yyyy}",
+                        NumberOfShards = 3,
+                        NumberOfReplicas = 1,
+                        AutoRegisterTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7, 
+                        FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                        EmitEventFailureHandling.WriteToFailureSink |
+                                        EmitEventFailureHandling.RaiseCallback,
+                        CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage:true)
+                    });
+                options.AddSerilog(loggerConfig.CreateLogger());
+            }).CreateLogger<Program>();
 
-            logger.Information("I am a person");
-            logger.Information("I am {@Person}", new 
+
+            logger.LogInformation("I am a person");
+            logger.LogInformation("I am {@Person}", new 
             {
                 FirstName = "Wajahat",
                 LastName = "Ali"
             });
-
-
-            await Task.Delay(2300);
+            try
+            {
+                throw new ArgumentException("Testing argument not found");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception happened");
+                logger.LogCritical(ex,"Argument exception happened");
+            }
+            
+            await Task.Delay(4000);
             Console.WriteLine("Hello World!");
         }
     }
