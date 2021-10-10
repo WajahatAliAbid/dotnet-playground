f8ecbacd17bc205d2d365c65befb6d84278f5211
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2021-10-10 14:46:18 +0500

    added basic logging support

---
+++ src/serilog-es/src/ESLogging/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+
+namespace ESLogging
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var loggerConfig = new LoggerConfiguration();
+            loggerConfig.WriteTo
+                .Console(new ElasticsearchJsonFormatter());
+            loggerConfig.WriteTo
+                .Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200/"))
+                {
+                    IndexFormat = "serilog-{0:yyyy}",
+                    NumberOfShards = 3,
+                    NumberOfReplicas = 1,
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7, 
+                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                       EmitEventFailureHandling.WriteToFailureSink |
+                                       EmitEventFailureHandling.RaiseCallback,
+                });
+            var logger = loggerConfig.CreateLogger();
+
+            logger.Information("I am a person");
+            logger.Information("I am {@Person}", new 
+            {
+                FirstName = "Wajahat",
+                LastName = "Ali"
+            });
+
+
+            await Task.Delay(2300);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
